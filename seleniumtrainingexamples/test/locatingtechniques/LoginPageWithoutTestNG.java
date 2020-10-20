5c0ae73959cf49491a3c5c5c52db8ee9366b1e34
Aaron Evans <aaron.evans@testery.io>
2020-10-20 00:05:46 -0600

    add resources and libraries needed to run tests cleanup project structure

--- seleniumtrainingexamples/test/locatingtechniques/LoginPageWithoutTestNG.java
+++ seleniumtrainingexamples/test/locatingtechniques/LoginPageWithoutTestNG.java
@@ -5,6 +5,8 @@
  */
 package locatingtechniques;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
