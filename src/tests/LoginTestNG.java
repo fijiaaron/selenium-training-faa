2b8bbe993adb91cb3ee491d870d6358d27c07371
Aaron Evans <aaron.evans@testery.io>
2021-04-21 12:59:11 -0600

    Refactor TestNG login test to use before/after methods and assertEquals

--- src/tests/LoginTestNG.java
+++ src/tests/LoginTestNG.java
@@ -1,24 +1,42 @@
 package tests;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTestNG
 {
-	@Test
-	public void successfulLogin()
+	WebDriver driver;
+	
+	@BeforeClass
+	public void init()
 	{
-		WebDriver driver;
-		
 		System.setProperty("webdriver.chrome.driver", "resources\\chromedriver.exe");
-
+	}
+	
+	@BeforeMethod
+	public void setup()
+	{	
 	    driver = new ChromeDriver();
 	 
 	    driver.get("https://the-internet.herokuapp.com/login");
+	}
+	
+	@AfterMethod
+	public void cleanup()
+	{
+	    driver.quit();
+	}
+	
+	@Test
+	public void successfulLogin()
+	{	
 	    driver.findElement(By.id("username")).sendKeys("tomsmith");
 	    driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
 	    driver.findElement(By.cssSelector(".fa")).click();
@@ -26,27 +44,20 @@ public class LoginTestNG
 	    String message1 = driver.findElement(By.id("flash")).getText();
 	    System.out.println(message1);
 	    
-	    assertTrue(message1.equals("Your password is invalid!\n"+ "×"));
+	    assertEquals(message1, "You logged into a secure area!\n"+ "×");
 	    
 	    driver.findElement(By.linkText("Logout")).click();
 	    String message2 = driver.findElement(By.id("flash")).getText();
 	    System.out.println(message2);
 
-	    
-	    driver.quit();
+	    assertEquals(message2, "You logged out of the secure area!\n"+ "×");
 	}
 	
 	
 	@Test
 	public void unsuccessfulLogin()
 	{
-		WebDriver driver;
-		
-		System.setProperty("webdriver.chrome.driver", "resources\\chromedriver.exe");
-
-	    driver = new ChromeDriver();
-	 
-	    driver.get("https://the-internet.herokuapp.com/login");
+ 
 	    driver.findElement(By.id("username")).sendKeys("tomsmith");
 	    driver.findElement(By.id("password")).sendKeys("SuperWrongPassword!");
 	    driver.findElement(By.cssSelector(".fa")).click();
@@ -54,9 +65,6 @@ public class LoginTestNG
 	    String message1 = driver.findElement(By.id("flash")).getText();
 	    System.out.println(message1);
 	    
-	    		
-	    assertTrue(message1.equals("Your password is invalid!\n"+ "×"));
-	    
-	    driver.quit();
+	    assertEquals(message1, "Your password is invalid!\n"+ "×");	
 	}
 }
