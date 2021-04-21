b39016f47da85fef92fcddb66d2233473d874233
Aaron Evans <aaron.evans@testery.io>
2021-04-21 14:45:31 -0600

    add getButtons test

---
+++ src/tests/ButtonTest.java
@@ -0,0 +1,44 @@
+package tests;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ButtonTest
+{
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setup()
+	{
+		System.setProperty("webdriver.chrome.driver", "resources\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+	}
+	
+	@AfterMethod
+	public void cleanup()
+	{
+		driver.quit();
+	}
+	
+	@Test
+	public void getAllTheButtons()
+	{
+		driver.get("https://www.faa.gov/");
+		List<WebElement> buttons = driver.findElements(By.tagName("button"));
+		System.out.println("number of buttons: " + buttons.size());
+		
+		for (WebElement button : buttons)
+		{
+			String text = button.getText();
+			System.out.println(text);
+		}
+	}
+}
