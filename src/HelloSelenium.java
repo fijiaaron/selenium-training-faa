13b8250a963287878ff4188ecdbdc7720e770a5f
Aaron Evans <aaron.evans@testery.io>
2021-04-21 09:04:31 -0600

    Add project to Github

---
+++ src/HelloSelenium.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HelloSelenium
+{
+	public static void main(String[] args)
+	{
+		WebDriver driver;
+		
+		System.out.println("We're going to start chrome with selenium....");
+		System.setProperty("webdriver.chrome.driver", "resources\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.faa.gov/");
+		driver.quit();
+		
+		System.out.println("We're going to start firefox with selenium....");
+		System.setProperty("webdriver.gecko.driver", "resources\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("https://www.faa.gov/");
+		driver.quit();
+	}
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		String name = "Aaron";
+		System.out.println("Hello, " + name);
+	}
+}
