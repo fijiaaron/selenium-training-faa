8852c43b2d84da96da6baffe8e276be821246c34
Aaron Evans <aarone@one-shore.com>
2021-08-12 07:04:46 -0600

    add cucumber reports

--- seleniumtrainingcucumber/src/test/java/oneshore/training/RunCucumberTest.java
+++ seleniumtrainingcucumber/src/test/java/oneshore/training/RunCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"})
 public class RunCucumberTest {
 
 }
